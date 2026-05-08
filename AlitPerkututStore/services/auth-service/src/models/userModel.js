a901cdaa535f5520ef5fd862d79a10ca6d600d1b
Alfarizy <arifalfrzy@gmail.com>
2026-05-08 21:54:18 +0700

    chore: userModel, first part

---
+++ AlitPerkututStore/services/auth-service/src/models/userModel.js
@@ -0,0 +1,27 @@
+import db from "../configs/db.js";
+
+export const findByEmail = async (email) => {
+  const [rows] = await db.query("SELECT * FROM users WHERE email = ?", [email]);
+  return rows[0];
+};
+
+export const findById = async (id) => {
+  const [rows] = await db.query("SELECT * FROM users WHERE id = ?", [id]);
+  return rows[0];
+};
+
+export const createUser = async ({ name, email, password, role }) => {
+  const [result] = await db.query(
+    `INSERT INTO users (name, email, password, role)
+     VALUES (?, ?, ?, ?)`,
+    [name, email, password, role],
+  );
+
+  const [rows] = await db.query(
+    `SELECT id, name, email, role FROM users WHERE id = ?`,
+    [result.insertId],
+  );
+
+  return rows[0];
+};
+
